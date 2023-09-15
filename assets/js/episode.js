a45b289213ed736b59f9158e1d77b69be208428b
Ale <soloalejo@gmail.com>
2023-09-15 16:38:56 +0200

    que mierda le pasa a esto

---
+++ assets/js/episode.js
@@ -0,0 +1,97 @@
+function displayEpisode (){
+    return `
+
+    <section id="cards_main">
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+          
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          
+          </section>
+      `
+}
